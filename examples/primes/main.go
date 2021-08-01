5f48948c7409020b14a481f317ada361c112e757
Alexander Karagulamos <karagulamos@gmail.com>
2021-08-01 23:06:26 +0100

    Added prime number generation example

---
+++ examples/primes/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"strconv"
+
+	"github.com/karagulamos/filterable"
+)
+
+func main() {
+	max := flag.String("max", "1000", "e.g. primes -max 100")
+
+	flag.Parse()
+
+	limit, err := strconv.Atoi(*max)
+
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	primes := filterable.
+		Range(2, limit).
+		Where(func(value interface{}) bool {
+			number := float64(value.(int))
+			return filterable.
+				Range(2, int(math.Sqrt(number))-1).
+				All(func(divisor interface{}) bool {
+					return int(number)%divisor.(int) != 0
+				})
+		})
+
+	fmt.Println(*primes)
+}
